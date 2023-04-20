38757d7eeb0f3818f095f066df2f7ca5cde2733f
hibara428 <hibara428@gmail.com>
2023-04-20 23:30:00 +0900

    Adds a profile option to infra shell scripts

--- infra/bin/deploy.sh
+++ infra/bin/deploy.sh
@@ -2,9 +2,11 @@
 # Deploy services.
 
 # Constants
-USAGE="Usage: ${0} create|update|delete HOSTED_ZONE_NAME ACM_CERTIFICATE_ARN"
+USAGE="Usage: ${0} create|update|delete HOSTED_ZONE_NAME ACM_CERTIFICATE_ARN <PROFILE>"
 STACK_NAME=roly-poly-counter
+BUCKET_NAME=${STACK_NAME}
 GITHUB_ORG_NAME=hibara428
+GITHUB_REPO_NAME=${STACK_NAME}
 TEMPLATE_FILE=./infra/cfn/template.yml
 
 # Parameters
@@ -15,21 +17,25 @@ fi
 action=${1}
 hosted_zone_name=${2}
 acm_certificate_arn=${3}
+profile_line=""
+if [ $# -eq 4 ]; then
+    profile_line="--profile ${4}"
+fi
 
 # CFn
 if [ "${action}" = "create" ] || [ "${action}" = "update" ]; then
-    aws cloudformation ${action}-stack \
+    aws cloudformation ${action}-stack ${profile_line} \
         --stack-name ${STACK_NAME} \
         --template-body file://${TEMPLATE_FILE} \
         --capabilities CAPABILITY_NAMED_IAM \
         --parameters \
-            ParameterKey=BucketName,ParameterValue="${STACK_NAME}" \
+            ParameterKey=BucketName,ParameterValue="${BUCKET_NAME}" \
             ParameterKey=HostedZoneName,ParameterValue="${hosted_zone_name}" \
             ParameterKey=AcmCertificateArn,ParameterValue="${acm_certificate_arn}" \
             ParameterKey=GitHubOrgName,ParameterValue="${GITHUB_ORG_NAME}" \
-            ParameterKey=GitHubRepoName,ParameterValue="${STACK_NAME}"
+            ParameterKey=GitHubRepoName,ParameterValue="${GITHUB_REPO_NAME}"
 elif [ "${action}" = "delete" ]; then
-    aws cloudformation delete-stack --stack-name "${STACK_NAME}"
+    aws cloudformation delete-stack ${profile_line} --stack-name "${STACK_NAME}"
 else
     echo "${USAGE}"
     exit 1
--- infra/bin/sync.sh
+++ infra/bin/sync.sh
@@ -4,6 +4,12 @@
 # Constants
 BUCKET_NAME=roly-poly-counter
 
+# Parameters
+profile_line=""
+if [ $# -ge 1 ]; then
+    profile_line="--profile ${1}"
+fi
+
 # Sync
-aws s3 sync ./dist s3://${BUCKET_NAME}/ --exclude ".DS_Store" --exclude "assets/*"
-aws s3 sync ./dist/assets s3://${BUCKET_NAME}/assets/ --exclude ".DS_Store" --delete
+aws s3 sync ${profile_line} ./dist s3://${BUCKET_NAME}/ --exclude ".DS_Store" --exclude "assets/*"
+aws s3 sync ${profile_line} ./dist/assets s3://${BUCKET_NAME}/assets/ --exclude ".DS_Store" --delete
